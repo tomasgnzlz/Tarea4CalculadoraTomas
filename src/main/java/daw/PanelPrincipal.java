e855903fa18a59282835138067716a7fa0d9f494
tgonati327 <tgonati327@g.educaand.es>
2024-04-22 09:15:47 +0200

    Inicio, codigo proporcionado por Vico

---
+++ src/main/java/daw/PanelPrincipal.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author tomas
+ */
+public class PanelPrincipal extends JPanel implements ActionListener {
+
+    // Atributos de la clase (privados)
+    private PanelBotones botonera;
+    private JTextArea areaTexto;
+    private int tipoOperacion;
+
+    // Constructor  
+    public PanelPrincipal() {
+        initComponents();
+        tipoOperacion = -1; // No hay operaciones en la calculadora
+        
+
+    }
+
+    // Se inicializan los componentes gráficos y se colocan en el panel
+    private void initComponents() {
+        // Creamos el panel de botones
+        botonera = new PanelBotones();
+        // Creamos el área de texto
+        areaTexto = new JTextArea(10, 50);
+        areaTexto.setEditable(false);
+        areaTexto.setBackground(Color.white);
+        //Establecemos layout del panel principal
+        this.setLayout(new BorderLayout());
+        // Colocamos la botonera y el área texto
+        this.add(areaTexto, BorderLayout.NORTH);
+        this.add(botonera, BorderLayout.SOUTH);
+        for (JButton boton : this.botonera.getgrupoBotones()) {
+            boton.addActionListener(this);
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        // Se obtiene el objeto que desencadena el evento
+        Object o = ae.getSource();
+        // Si es un botón
+        if (o instanceof JButton) {
+            System.out.println(((JButton) o).getText());
+            areaTexto.setText(((JButton) o).getText());
+        }
+        // RESTO DEL CÓDIGO DE LA LÓGICA DE LA CALCULADORA
+    }
+
+}
